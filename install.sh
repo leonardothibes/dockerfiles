2ddffd72c179b7f74bbce2f24bc764709da21c29
Leonardo Thibes <leonardothibes@gmail.com>
2017-11-20 12:36:40 -0200

    wip

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 clear
-echo "Installing..."
 
-for
+if [ "$(which docker)" == "" ]; then
+    echo "Docker not found!"
+    exit 1
+fi;
+
+BIN="${HOME}/.bin"
+HERE=$(pwd)
+
+for CONTAINER in $(ls containers)
+do
+    EXEC="${HERE}/containers/${CONTAINER}/exec.sh"
+    if [ -f ${EXEC} ]; then
+        echo "Instaling ${CONTAINER}..."
+        # ln -sf ${BIN}/${CONTAINER} ${EXEC}
+    fi;
+done;
