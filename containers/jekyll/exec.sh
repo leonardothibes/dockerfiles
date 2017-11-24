787617140581174055aff165c92cf32e92891991
Leonardo Thibes <leonardothibes@gmail.com>
2017-11-24 17:18:05 -0200

    Fixing #1

--- containers/jekyll/exec.sh
+++ containers/jekyll/exec.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run --rm --volume=$(pwd):/var/www -it -p 4000:4000 leonardothibes/jekyll jekyll $@
+SYSTEM=$(uname)
+DOCKER=$(which docker)
+
+if [ $SYSTEM == "Linux" ]; then
+    DOCKER="sudo ${DOCKER}"
+fi
+
+${DOCKER} run --rm --volume=$(pwd):/var/www -it -p 4000:4000 leonardothibes/jekyll jekyll $@
