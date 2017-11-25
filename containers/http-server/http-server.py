cdb678b1b301fedff1961ec8f2462c1557dd9544
Leonardo Thibes <leonardothibes@gmail.com>
2017-11-24 23:39:40 -0200

    Script Python para rodar o server

---
+++ containers/http-server/http-server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import SimpleHTTPServer
+
+class MyHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_my_headers()
+
+        SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_my_headers(self):
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Cache-Control", "max-age=0")
+
+
+if __name__ == '__main__':
+    SimpleHTTPServer.test(HandlerClass=MyHTTPRequestHandler)
